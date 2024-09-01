6a9cdeca991ffcadcdd972789521b37d79a5b60f
Ryo Iida <ryoaksum@gmail.com>
2024-09-01 17:35:58 +0900

    add sanctum with default setting

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+        return response()->json([
+            'message' => 'hello world'
+        ]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ProductController;
+use App\Http\Controllers\Api\UserController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-// Route::post('/mylogin', [MemberController::class, 'login']);
+Route::post('/login', UserController::class)->name('login');
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/products', ProductController::class);
