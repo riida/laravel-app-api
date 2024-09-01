6ad2257092f1c9a177c5a464f53130d8c62b965c
Ryo Iida <ryoaksum@gmail.com>
2024-09-01 17:21:25 +0900

    add products endpoint which needs authentication

---
+++ app/Http/Controllers/Api/ProductController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+        $products = Product::all();
+        return response()->json([
+            'status' => true,
+            'products' => $products
+        ]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ProductController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+// Route::post('/mylogin', [MemberController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/products', ProductController::class);
 });
