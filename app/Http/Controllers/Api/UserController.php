4f72192cd63716753b90f759a0005974e2b01bd4
Ryo Iida <ryoaksum@gmail.com>
2024-09-02 00:19:06 +0900

    implement authenticaion

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
@@ -12,6 +14,10 @@ class UserController extends Controller
      */
     public function __invoke(Request $request)
     {
+        $user = User::factory()->create();
+        Auth::loginUsingId($user->id);
+        $user->createToken('test');
+
         return response()->json([
             'message' => 'hello world'
         ]);
