46cd794f09ceecc1b75851cefd07ce61b3ccafac
Ryo Iida <ryoaksum@gmail.com>
2024-09-02 09:41:28 +0900

    add firebase auth

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -6,15 +6,43 @@ use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
+use Kreait\Firebase\Contract\Auth as FirebaseAuth;
+use Kreait\Firebase\Exception\Auth\FailedToVerifyToken;
 
 class UserController extends Controller
 {
+    public function __construct(
+        private readonly FirebaseAuth $auth,
+    ) {}
+
     /**
      * Handle the incoming request.
      */
     public function __invoke(Request $request)
     {
-        $user = User::factory()->create();
+        $id_token = $request->id_token;
+
+        try {
+            $verified_id_token = $this->auth->verifyIdToken($id_token);
+        } catch (FailedToVerifyToken $e) {
+            Log::error("token is invalid: " . $e);
+            return response()->json([
+                'message' => 'Token is invalid'
+            ], 400);
+        }
+
+        $firebase_uid = $verified_id_token->claims()->get('sub');
+        Log::info('firebase_uid: ' . $firebase_uid);
+        $user = User::where('firebase_uid', $firebase_uid)->first();
+        if (!$user) {
+            Log::error('user is not found');
+            return response()->json([
+                'message' => 'Token is invalid'
+            ], 400);
+        }
+        // $user = User::factory()->create();
+
         Auth::loginUsingId($user->id);
 
         return response()->json([
